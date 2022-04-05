b9fb64f0704ea1dfc7eac8851f802f7aae3e7805
Marlon Liu <Marlon.Liu@ibm.com>
2022-04-05 11:07:10 -0400

    change homepage

--- frontend/cake404/src/pages/Home.jsx
+++ frontend/cake404/src/pages/Home.jsx
@@ -1,12 +1,69 @@
 import React from "react";
 import { Component } from "react";
+import Box from "@mui/material/Box"
+import { createTheme, IconButton, iconButtonClasses } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
 
 class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {themeOn : true}
+    }
+
+    theme1 = createTheme({
+        palette: {
+            background: {
+                default: "#ED6A5A"
+            },
+            text: {
+                primary: "#F0F8FF"
+            }
+        }
+    })
+
+    theme2 = createTheme({
+        palette: {
+            background: {
+                default: "#F4F1BB"
+            },
+            text: {
+                primary: "#000000"
+            }
+        }
+    })
+
+
+    changeTheme = () => {
+        this.setState({themeOn: !this.state.themeOn})
+        console.log(this.state.themeOn)
+    }
+
     render() {
         return (
-            <div>
-                <h1>Hello world!</h1>
-            </div>
+            <ThemeProvider theme={this.state.themeOn ? this.theme1 : this.theme2}>
+                <CssBaseline />
+                <div className="home" >
+                    <Box sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        width: "100%",
+                        textAlign: "center",
+                        justifyContent: 'center',
+                    }}>
+                        <div>
+                            <h1>Cake 404</h1>
+                            <p>Bringing you the alternate dessert-'ive' experience</p>
+                        </div>
+                        <IconButton sx={{ ml: 1 }} onClick={this.changeTheme} color="inherit">
+                            {this.state.themeOn ? <Brightness7Icon /> : <Brightness4Icon />}
+                        </IconButton>
+                    </Box>
+                </div>
+            </ThemeProvider>
         )
     }
 }
