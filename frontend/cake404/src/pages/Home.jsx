158a9340492d95b73641c897429982a943cf0513
Marlon Liu <Marlon.Liu@ibm.com>
2022-04-07 11:24:13 -0400

    test commit

--- frontend/cake404/src/pages/Home.jsx
+++ frontend/cake404/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import NavBar from "../components/NavBar";
+import {Box} from "@mui/material"
 
 
 class Home extends Component {
